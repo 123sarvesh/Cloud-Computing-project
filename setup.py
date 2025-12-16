66231a3258edbcce751700305c4136cb295e4ce2
Sarvesh Gupta <78999139+123sarvesh@users.noreply.github.com>
2025-12-16 11:20:44 -0500

    setup.py
    Removed header comments from setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
-# =============================================================================
-# @file    setup.py
-# @brief   Installation setup file
-# @author  Michael Hucka <mhucka@caltech.edu>
-# @license Please see the file named LICENSE in the project directory
-# @website https://github.com/caltechlibrary/handprint
-#
-# Note: configuration metadata is maintained in setup.cfg.  This file exists
-# primarily to hook in setup.cfg and requirements.txt.
-#
-# =============================================================================
-
 import os
 from   os.path import exists, join, abspath, dirname
 from   setuptools import setup
